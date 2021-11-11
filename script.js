0355a846ef08d418cb555f598c63fa5f2438fcdc
Silveron1969 <57366587+Silveron1969@users.noreply.github.com>
2021-11-11 19:42:56 +0300

    Added script.js and style guests block

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector(".button_visit___select_visit__blocknumber").onclick = function () {
+  document.querySelector(".guests-choice___select_visit__blocknumber").style.display = "block";
+  document.querySelector(".button_visit___select_visit__blocknumber").style.border = "1px solid rgba(31, 32, 65, 0.5)";
+  document.querySelector(".button_visit___select_visit__blocknumber").style.borderRadius = "4px 4px 0px 0px";
+  document.querySelector(".button_visit___select_visit__blocknumber").style.borderBottom = "0px";
+};
+ 
