310b4a28140911a7e3ca555f54a57592e3f62417
Silveron1969 <57366587+Silveron1969@users.noreply.github.com>
2021-11-11 19:53:39 +0300

    Position absolute block guests

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.querySelector(".button_visit___select_visit__blocknumber").onclick = function () {
+function DispButtonGuests() {
   document.querySelector(".guests-choice___select_visit__blocknumber").style.display = "block";
   document.querySelector(".button_visit___select_visit__blocknumber").style.border = "1px solid rgba(31, 32, 65, 0.5)";
   document.querySelector(".button_visit___select_visit__blocknumber").style.borderRadius = "4px 4px 0px 0px";
