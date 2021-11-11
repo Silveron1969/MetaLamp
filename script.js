cca2669f7349e243a24ac7b7ffe45e7c0978467a
Silveron1969 <57366587+Silveron1969@users.noreply.github.com>
2021-11-11 21:33:28 +0300

    Added a script for the number of guests

--- script.js
+++ script.js
@@ -4,4 +4,74 @@ function DispButtonGuests() {
   document.querySelector(".button_visit___select_visit__blocknumber").style.borderRadius = "4px 4px 0px 0px";
   document.querySelector(".button_visit___select_visit__blocknumber").style.borderBottom = "0px";
 };
- 
+/*кнопки плюс минус*/
+function ClickButtonMinusAdults() {
+  var per = parseFloat(document.querySelector(".adults-text").innerHTML) - 1;
+  if (per == 0) document.querySelector(".adults_minus").style.opacity = 0.38;
+  if (per < 0) return 0;
+  document.querySelector(".adults-text").innerHTML = per;
+  var guests = parseFloat(document.querySelector(".the-guests").innerHTML) - 1;
+  var word;
+  if (guests == "1") word = "гость";
+  if (guests >= 2 && guests <= 4) word = "гостя";
+  if (guests == 0 || guests >= 5) word = "гостей";
+  document.querySelector(".the-guests").innerHTML = guests + " " + word;
+}
+function ClickButtonPlusAdults() {
+  var per = parseFloat(document.querySelector(".adults-text").innerHTML) + 1;
+  if (per > 0) document.querySelector(".adults_minus").style.opacity = 1;
+  document.querySelector(".adults-text").innerHTML = per;
+  var guests = parseFloat(document.querySelector(".the-guests").innerHTML) + 1;
+  var word;
+  if (guests == "1") word = "гость";
+  if (guests >= 2 && guests <= 4) word = "гостя";
+  if (guests == 0 || guests >= 5) word = "гостей";
+  document.querySelector(".the-guests").innerHTML = guests + " " + word;
+}
+function ClickButtonMinusChildren() {
+  var per = parseFloat(document.querySelector(".children-text").innerHTML) - 1;
+  if (per == 0) document.querySelector(".children_minus").style.opacity = 0.38;
+  if (per < 0) return 0;
+  document.querySelector(".children-text").innerHTML = per;
+  var guests = parseFloat(document.querySelector(".the-guests").innerHTML) - 1;
+  var word;
+  if (guests == "1") word = "гость";
+  if (guests >= 2 && guests <= 4) word = "гостя";
+  if (guests == 0 || guests >= 5) word = "гостей";
+  document.querySelector(".the-guests").innerHTML = guests + " " + word;
+}
+function ClickButtonPlusChildren() {
+  var per = parseFloat(document.querySelector(".children-text").innerHTML) + 1;
+  if (per > 0) document.querySelector(".children_minus").style.opacity = 1;
+  document.querySelector(".children-text").innerHTML = per;
+  var guests = parseFloat(document.querySelector(".the-guests").innerHTML) + 1;
+  var word;
+  if (guests == "1") word = "гость";
+  if (guests >= 2 && guests <= 4) word = "гостя";
+  if (guests == 0 || guests >= 5) word = "гостей";
+  document.querySelector(".the-guests").innerHTML = guests + " " + word;
+}
+function ClickButtonMinusBabies() {
+  var per = parseFloat(document.querySelector(".babie-text").innerHTML) - 1;
+  if (per == 0) document.querySelector(".babies_minus").style.opacity = 0.38;
+  if (per < 0) return 0;
+  document.querySelector(".babie-text").innerHTML = per;
+  var guests = parseFloat(document.querySelector(".the-guests").innerHTML) - 1;
+  var word;
+  if (guests == "1") word = "гость";
+  if (guests >= 2 && guests <= 4) word = "гостя";
+  if (guests == 0 || guests >= 5) word = "гостей";
+  document.querySelector(".the-guests").innerHTML = guests + " " + word;
+}
+function ClickButtonPlusBabies() {
+  var per = parseFloat(document.querySelector(".babie-text").innerHTML) + 1;
+  if (per > 0) document.querySelector(".babies_minus").style.opacity = 1;
+  document.querySelector(".babie-text").innerHTML = per;
+  var guests = parseFloat(document.querySelector(".the-guests").innerHTML) + 1;
+  var word;
+  if (guests == "1") word = "гость";
+  if (guests >= 2 && guests <= 4) word = "гостя";
+  if (guests == 0 || guests >= 5) word = "гостей";
+  document.querySelector(".the-guests").innerHTML = guests + " " + word;
+}
+
